58a150bcc0b8a2a8aa7ccb8f1f8b133afa751bdf
Fabi2019 <fabivilla2019@gmail.com>
2026-03-04 09:45:10 -0600

    agrega juegos

--- copia.js
+++ copia.js
@@ -1,57 +1,77 @@
 {
-    "configuracion": {
-        "titulo_pagina": "Fabián Matemática",
-        "menu": [
-            { "texto": "Programas", "clave": "programas" },
-            { "texto": "Prácticas", "clave": "practicas" },
-            { "texto": "Avisos MEP", "clave": "avisomep" }
+"configuracion": {
+"titulo_pagina": "Fabián Matemática",
+"menu": [
+    {
+    "texto": "Programas",
+    "clave": "programas"
+    },
+    {
+    "texto": "Prácticas",
+    "clave": "practicas"
+    },
+    { "texto": "Avisos MEP",
+    "clave": "avisomep"},
+    { "texto": "Juegos",
+    "clave": "juego"}
+]
+},
+    "programas": [
+        {
+        "nombre": "Contenido 7, Página 3 (descargar)",
+        "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/Tabla-III-Ciclo.pdf"
+        },
+        {
+        "nombre": "Contenido 8, Página 9 (descargar)",
+        "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/Tabla-III-Ciclo.pdf"
+        },
+        {
+        "nombre": "Contenido 9, Página 14 (descargar)",
+        "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/Tabla-III-Ciclo.pdf"
+        },
+        {
+        "nombre": "Contenido BXM (descargar)",
+        "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/Tabla-de-BxM-1.pdf"
+        }
+        ],
+    "practicas": [
+        {
+        "nombre": "Práctica Examen 7mo (descargar)",
+        "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/PRACTICA-SETIMO-MATEMATICAS.pdf"
+        },
+        {
+        "nombre": "Práctica Examen 8vo (descargar)",
+        "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/OCTAVO-PRACTICA.pdf"
+        },
+        {
+        "nombre": "Práctica Examen 9no (descargar)",
+        "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/PRACTICA-NOVENO-MATEMATICAS.pdf"
+        },
+        {
+        "nombre": "Práctica Examen BXM (descargar)",
+        "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/BXM-PRACTICA.pdf"
+        }
+        ],
+    "avisomep": [
+        {
+        "nombre": "Tercer Ciclo:",
+        "url": "https://dgec.mep.go.cr/iii-ciclo/#:~:text=Avisos"
+        },
+        {
+        "nombre": "BXM:",
+        "url": "https://dgec.mep.go.cr/bachillerato-por-madurez-suficiente/#:~:text=Avisos"
+        }
+        ],
+    "juego": [
+        {
+        "nombre": "Operaciones con Enteros:",
+        "url": "https://fabi2019.github.io/OperEntero/"
+        },
+        {
+        "nombre": "Raices:",
+        "url": "https://fabi2019.github.io/PotYRaiz/"
+        }
         ]
-    },
-"programas": [
-    {
-    "nombre": "Contenido 7, Página 3 (descargar)",
-    "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/Tabla-III-Ciclo.pdf"
-    },
-    {
-    "nombre": "Contenido 8, Página 9 (descargar)",
-    "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/Tabla-III-Ciclo.pdf"
-    },
-    {
-    "nombre": "Contenido 9, Página 14 (descargar)",
-    "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/Tabla-III-Ciclo.pdf"
-    },
-    {
-    "nombre": "Contenido BXM (descargar)",
-    "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/Tabla-de-BxM-1.pdf"
-    }
-    ],
-"practicas": [
-    {
-    "nombre": "Práctica Examen 7mo (descargar)",
-    "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/PRACTICA-SETIMO-MATEMATICAS.pdf"
-    },
-    {
-    "nombre": "Práctica Examen 8vo (descargar)",
-    "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/OCTAVO-PRACTICA.pdf"
-    },
-    {
-    "nombre": "Práctica Examen 9no (descargar)",
-    "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/PRACTICA-NOVENO-MATEMATICAS.pdf"
-    },
-    {
-    "nombre": "Práctica Examen BXM (descargar)",
-    "url": "https://dgec.mep.go.cr/wp-content/uploads/2026/02/BXM-PRACTICA.pdf"
-    }
-    ],
-"avisomep": [
-    {
-    "nombre": "Tercer Ciclo:",
-    "url": "https://dgec.mep.go.cr/iii-ciclo/#:~:text=Avisos"
-    },
-    {
-    "nombre": "BXM:",
-    "url": "https://dgec.mep.go.cr/bachillerato-por-madurez-suficiente/#:~:text=Avisos"
-    }
-    ]
-}
-
+    
+    
+        }
